badbd2604fb5ae860b4925fa902ddd83f595efd1
Mahalakshmi S <mahasadha201@gmail.com>
2024-03-28 14:27:35 +0530

    welcome msg

--- sample.java
+++ sample.java
@@ -1,7 +1,6 @@
 public class sample
  {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("welcome to jenkins auto");
     }
 }
-
